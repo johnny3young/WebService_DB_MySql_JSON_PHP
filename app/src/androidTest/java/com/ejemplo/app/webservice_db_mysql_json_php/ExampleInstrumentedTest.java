e3877ddd5f94614e7499c898c6d9fa1a3c30ad17
johnny3young <youngerblack@gmail.com>
2019-06-10 13:23:29 -0500

    Sending first part project, only interface

---
+++ app/src/androidTest/java/com/ejemplo/app/webservice_db_mysql_json_php/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.ejemplo.app.webservice_db_mysql_json_php;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.ejemplo.app.webservice_db_mysql_json_php", appContext.getPackageName());
+    }
+}
